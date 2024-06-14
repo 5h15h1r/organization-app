8842632815910c8d33de6e5c0572d03b40990529
5h15h1r <shishir.agrahari14@gmail.com>
2024-06-14 11:46:52 +0530

    feat: add support for environment variables

--- config/test.exs
+++ config/test.exs
@@ -6,10 +6,10 @@ import Config
 # to provide built-in test partitioning in CI environment.
 # Run `mix help test` for more information.
 config :organization_api, OrganizationApi.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "organization_api_test#{System.get_env("MIX_TEST_PARTITION")}",
+  username: System.get_env("POSTGRES_USERNAME"),
+  password: System.get_env("POSTGRES_PASSWORD"),
+  hostname: System.get_env("POSTGRES_HOSTNAME"),
+  database: "#{System.get_env("POSTGRES_TEST_DATABASE")}#{System.get_env("MIX_TEST_PARTITION")}",
   pool: Ecto.Adapters.SQL.Sandbox,
   pool_size: System.schedulers_online() * 2
 
