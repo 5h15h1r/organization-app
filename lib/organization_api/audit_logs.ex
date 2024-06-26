b3f9513d74bd8b9e961b43e724b0de2d90126fc7
5h15h1r <shishir.agrahari14@gmail.com>
2024-06-26 17:46:21 +0530

    refactor: add docs for all the functions in audit_log context

--- lib/organization_api/audit_logs.ex
+++ lib/organization_api/audit_logs.ex
@@ -9,12 +9,12 @@ defmodule OrganizationApi.AuditLogs do
   alias OrganizationApi.Workers.AuditLogWorker
 
   @doc """
-  Create audti log .
+  Create an audit log .
 
   ## Examples
 
-      iex> create_audit_log()
-      [%Audtit{}, ...]
+      iex> create_audit_log(%{action: "create", table_name: "organizations", new_data: %{name: "Org1"}, old_data: nil})
+      [%AuditLog{}, ...]
 
   """
   def create_audit_log(attrs \\ %{}) do
@@ -23,17 +23,45 @@ defmodule OrganizationApi.AuditLogs do
     |> Oban.insert()
   end
 
+  @doc """
+  Lists all audit logs.
+
+  ## Examples
+
+      iex> list_audit_log()
+      [%AuditLog{}, ...]
+
+  """
   def list_audit_log do
     AuditLog
     |> Repo.all()
   end
 
+
+  @doc """
+  Filters audit logs by table name.
+
+  ## Examples
+
+      iex> filter_audit_logs_by_table("organizations")
+      [%AuditLog{}, ...]
+
+  """
   def filter_audit_logs_by_table(table_name) do
     AuditLog
     |> where(table_name: ^table_name)
     |> Repo.all()
   end
 
+@doc """
+  Filters audit logs by action.
+
+  ## Examples
+
+      iex> filter_audit_logs_by_action("create")
+      [%AuditLog{}, ...]
+
+  """
   def filter_audit_logs_by_action(action) do
     AuditLog
     |> where(action: ^action)
